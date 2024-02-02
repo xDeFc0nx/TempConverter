964a16d0474b59c600e4417228a4d955a4350466
xDeFc0nx <nehartale8@gmail.com>
2024-02-02 22:43:49 +0100

    formated

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
-
 int calc;
 float fahr, celsius, numb, result;
 
-int main()
-{
- printf("\nIs this C or F \n");
-   scanf("%c", &calc);
+int main() {
+  printf("\nIs this C or F \n");
+  scanf("%c", &calc);
 
-printf("\nProvide the number \n");
+  printf("\nProvide the number \n");
   scanf("%f", &numb);
 
   if (calc == 'C') {
 
-  result = (numb * 9.0/5.0) + 32.0;
-
-
-    printf("%6.1f %3.0f\n", result, numb);
-}
-if (calc == 'F'){
-    result = (numb - 32.0) * 5.0/9.0;
+    result = (numb * 9.0 / 5.0) + 32.0;
 
     printf("%6.1f %3.0f\n", result, numb);
-
   }
-return 0;
+  if (calc == 'F') {
+    result = (numb - 32.0) * 5.0 / 9.0;
+
+    printf("%6.1f %3.0f\n", result, numb);
+  }
+  return 0;
 }
